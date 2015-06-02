f942d0bf91dabccbb7c28843ec449a1252ee9fe6
Hamed KHANDAN <hkhandan@ieee.org>
2015-06-02 15:37:09 +0900

    PicServer

--- src/demo/PicServer.kap/PicServerAgent.h
+++ src/demo/PicServer.kap/PicServerAgent.h
@@ -11,15 +11,54 @@
 
 using namespace knorba;
 using namespace knorba::type;
-
+using namespace knorba::protocol;
+using namespace knorba::demo;
 
 class PicServerAgent : public Agent {
 
+// --- NESTED TYPES --- //
+  
+  private: class PGrouping : public GroupingProtocol {
+    private: PicServerAgent* _iAgent;
+    public: PGrouping(PicServerAgent* owner);
+    public: void onAllMembersConnected();
+  };
+  
+  
+  private: class JarRunnerThread : public Thread {
+    private: PicServerAgent* _iAgent;
+    public: JarRunnerThread(PicServerAgent* agent);
+    public: void run();
+  };
+
+  
 // --- STATIC FIELDS --- //
   
+  private: static const int READ_END = 0;
+  private: static const int WRITE_END = 1;
   public: static const SPtr<KString> OP_START;
+  public: static const SPtr<KString> R_MATE;
+  
+  
+// --- STATIC METHODS --- //
+  
+  static char* cstr(const string& str) {
+    char* buffer = new char[str.length() + 1];
+    memcpy(buffer, str.c_str(), str.length() + 1);
+    return buffer;
+  }
   
 
+// --- FIELDS --- //
+  
+  private: DisplayInfoProtocol _pDisplayInfo;
+  private: Ptr<JarRunnerThread> _jarRunnerThread;
+  private: int _pipe[2];
+  private: int _childId;
+  private: bool _uiReady;
+  private: bool _isWindowSet;
+  
+  
 // --- (DE)CONSTRUCTORS --- //
   
   public: PicServerAgent(Runtime& rt, const k_guid_t& guid);
@@ -27,8 +66,18 @@ class PicServerAgent : public Agent {
   
 // --- METHODS --- //
   
+  private: void runJar();
+  private: void writeToPipe(const string& str);
+  
   // Handlers //
   public: void handleOpStart(PPtr<Message> msg);
+  public: void handleOpLoad(PPtr<Message> msg);
+  public: void handleOpPut(PPtr<Message> msg);
+  public: void handleOpUnput(PPtr<Message> msg);
+  
+  // Inherited from Agent //
+  public: void finalize();
+  public: bool isAlive();
   
 };
 
