bfc213f5c5cc9536c0eb3ac2720e884c84ab0e6a
Hamed KHANDAN <hkhandan@ieee.org>
2015-06-03 21:07:43 +0900

    _agentQuitCond was made not shared, because shared mode causes blockage

--- src/are/RuntimeBase.cpp
+++ src/are/RuntimeBase.cpp
@@ -43,7 +43,7 @@ namespace are {
    * Primary constructor
    */
   RuntimeBase::RuntimeBase(PPtr<Path> knorbaHome, const k_guid_t& guid)
-  : _agentQuitCond(true)
+  : _agentQuitCond(false)
   {
     _nodeCount = 1;
     _closing = false;
